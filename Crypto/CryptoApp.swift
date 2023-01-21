c497df60a99400f6098699ece1d331b41dff9205
PIYUSH SINGH <piyushsingh9862@gmail.com>
2023-01-22 01:38:48 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Piyush Singh on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
